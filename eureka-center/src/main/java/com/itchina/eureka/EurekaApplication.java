aab89c9d0d3a047662a9451f5a5e82d90ca8c135
xiadongming <1816808381@qq.com>
2021-04-15 22:23:07 +0800

    注册中心

---
+++ eureka-center/src/main/java/com/itchina/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.itchina.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * @Date: 2021/4/6 22:30
+ * @Desc:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
